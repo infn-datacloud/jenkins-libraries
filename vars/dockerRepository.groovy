736203a9ca59ecaf3e92872bb4a8be94916a5371
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-10-12 15:43:49 +0000

    chore: Use env to retrieve branch name and commit ID.

--- vars/dockerRepository.groovy
+++ vars/dockerRepository.groovy
@@ -1,11 +1,11 @@
 void pushImage(String srcImage, String targetImageName, String registryUrl, String registryCredentialsName) {
     sh "docker tag ${srcImage} ${targetImageName}"
     docker.withRegistry("${registryUrl}", "${registryCredentialsName}") {
-        if ("${BRANCH_NAME}" == 'main') {
+        if ("${env.BRANCH_NAME}" == 'main') {
             docker.image("${targetImageName}").push('latest')
         }
-        docker.image("${targetImageName}").push("${BRANCH_NAME}")
-        docker.image("${targetImageName}").push("${COMMIT_SHA}")
+        docker.image("${targetImageName}").push("${env.BRANCH_NAME}")
+        docker.image("${targetImageName}").push("${env.GIT_COMMIT}")
     }
 }
 
