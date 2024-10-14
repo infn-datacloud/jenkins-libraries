0e9ce0ab9d78716aadc690e1aeedb5aefc249510
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-10-14 12:51:02 +0000

    refactor: use ${} for variables

--- vars/pythonProject.groovy
+++ vars/pythonProject.groovy
@@ -1,9 +1,10 @@
-void formatCode(String pythonVersion, String srcDir) {
-    docker.image("ghcr.io/withlogicco/poetry:1.8.3-python-$pythonVersion-slim")
-        .inside('-e POETRY_VIRTUALENVS_IN_PROJECT=true -u root:root') {
-            sh 'poetry install'
-            sh "ruff check ./$srcDir"
-            sh 'ruff format --check .'
+void formatCode(String pythonVersion = "3.12", String srcDir = "src") {
+    docker
+    .image("ghcr.io/withlogicco/poetry:1.8.3-python-${pythonVersion}-slim")
+    .inside('-e POETRY_VIRTUALENVS_IN_PROJECT=true -u root:root') {
+        sh 'poetry install'
+        sh "ruff check ${srcDir}"
+        sh 'ruff format --check .'
     }
 }
 
