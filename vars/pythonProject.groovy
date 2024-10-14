b70491230bd8223a9b8e1c610c145dc96c81e360
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2024-10-14 12:48:21 +0000

    chore: add function to test python code

--- vars/pythonProject.groovy
+++ vars/pythonProject.groovy
@@ -6,3 +6,19 @@ void formatCode(String pythonVersion, String srcDir) {
             sh 'ruff format --check .'
     }
 }
+
+void testCode(String dockerArgs = '', String coverageDir = 'coverage-reports') {
+    docker
+    .image("ghcr.io/withlogicco/poetry:1.8.3-python-${pythonVersion}-slim")
+    .inside("""-e POETRY_VIRTUALENVS_IN_PROJECT=true -u root:root ${dockerArgs}""") {
+        sh 'poetry install'
+        configFileProvider([configFile(fileId:  '.coveragerc', variable: 'COVERAGERC')]) {
+                sh """poetry run pytest \
+                --resetdb \
+                --cov \
+                --cov-config=${COVERAGERC} \
+                --cov-report=xml:${coverageDir}/coverage-${pythonVersion}.xml \
+                --cov-report=html:${coverageDir}/htmlcov-${pythonVersion}"""
+        }
+    }
+}
