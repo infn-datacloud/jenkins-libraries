e7ae7f76b93aacbf8fcef55f9dcfe4142b42119a
Giovanni Savarese <giovanni.savarese@lnl.infn.it>
2025-02-21 10:23:51 +0000

    fix: change map merging everywhere. Add substring check also in non-main branch

--- vars/dockerRepository.groovy
+++ vars/dockerRepository.groovy
@@ -1,7 +1,6 @@
 /* groovylint-disable-next-line BuilderMethodWithSideEffects, FactoryMethodName */
 Object buildImage(Map args) {
-    Map kwargs = [pythonVersion: '', customTags: []]
-    kwargs << args
+    Map kwargs = [pythonVersion: '', customTags: []] + args
     String buildArgs = ''
     String name = kwargs.imageName
     if ("${kwargs.pythonVersion}" != '') {
@@ -22,21 +21,22 @@ void pushImage(Map args) {
     docker.withRegistry("${kwargs.registryUrl}", "${kwargs.registryCredentialsName}") {
         if ("${env.BRANCH_NAME}" == 'main') {
             String gitTag = sh(returnStdout: true, script: 'git tag --sort version:refname | tail -1').trim()
-            kwargs.srcImage.push()
             if (imageTag =~ kwargs.latestMatch) {
                 kwargs.srcImage.push('latest')
                 kwargs.srcImage.push("${gitTag}")
             }
             kwargs.srcImage.push("${gitTag}-${imageTag}")
         } else {
+            if (imageTag =~ kwargs.latestMatch) {
+                kwargs.srcImage.push("${env.BRANCH_NAME}")
+            }
             kwargs.srcImage.push("${env.BRANCH_NAME}-${imageTag}")
         }
     }
 }
 
 void updateReadMe(Map args) {
-    Map kwargs = [registryType: 'dockerhub']
-    kwargs << args
+    Map kwargs = [registryType: 'dockerhub'] + args
     String imageName = kwargs.srcImage.imageName()
     sh """docker run --rm \
         -v ${WORKSPACE}:/myvol \
@@ -52,8 +52,7 @@ void updateReadMe(Map args) {
 
 /* groovylint-disable-next-line BuilderMethodWithSideEffects, FactoryMethodName */
 void buildAndPushImage(Map args) {
-    Map kwargs = [registryType: 'dockerhub', pythonVersion: '', customTags: []]
-    kwargs << args
+    Map kwargs = [registryType: 'dockerhub', pythonVersion: '', customTags: []] + args
     Object img
     stage('Build image') {
         img = buildImage(
